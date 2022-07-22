25646ddd094018a4bac0d2f2491387139bfd0618
abdullatef trefi <abdullatef.trefi98@gmail.com>
2022-07-22 18:15:55 +0300

    g

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const server = http.createServer((req,res)=>{
    if(req.url == "/"){
-        fs.readFile('form.html',{encoding:'utf-8'},(data) => {
-            res.write(data);
+        fs.readFile('form.html',{encoding:'utf-8'},(err,resp) => {
+            res.write(resp);
             res.end();
         })
    }
