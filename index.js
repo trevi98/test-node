84072708fc3d039470308485d96a44e2540d938a
abdullatef trefi <abdullatef.trefi98@gmail.com>
2022-07-22 17:27:41 +0300

    f

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req,res)=>{
-    res.writeHead(200);
-    res.write("hello wrold")
-    res.end()
+   if(req.url == "/"){
+        fs.readFile('form.html',{encoding:'utf-8'},(data) => {
+            res.write(data);
+            res.end();
+        })
+   }
+   else{
+        console.log(req);
+        fs.writeFile('log.txt',req,{encoding:'utf-8'},()=>{console.log('done')})
+
+   }
 });
 
 server.listen(process.env.PORT || 5000);
